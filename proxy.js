02d3066cfa6439d948f592bb41ca13cfdda3ada9
recitativonika <fool-body-cheddar@duck.com>
2024-10-16 23:02:47 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 module.exports = [
     'http://proxy1:port',
     'http://proxy2:port',
-	'proxy:port',
-	'https://user:pass@proxy:port',
+    'proxy:port',
+    'https://user:pass@proxy:port',
 	// change all this with your proxy if you want to use proxy
 ];
