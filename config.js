b25bc49f7337d1479d040d96415c6603ae23d0f4
recitativonika <fool-body-cheddar@duck.com>
2024-10-18 06:50:31 +0700

    Add costum delay before processing the next account

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
     useProxy: false, // Set to true if you want to run with proxies
     minDelay: 3, // random delay for keepalive packet send
     maxDelay: 10, // random delay for keepalive packet send
-    restartDelay: 160, // Delay in seconds before restarting the process
-	accountDelay: 30, // Custom delay in seconds before processing the next account
+    restartDelay: 241, // Delay in seconds before restarting the process
+    accountDelay: 121, // Custom delay in seconds before processing the next account
 };
